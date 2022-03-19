29dfd85d70bd88288a5c4a7b35d38af716a888e3
Lepre lepreeeee <i82bocar@uco.es>
2022-03-19 15:11:02 +0100

    Creamos fichero para algoritmo turi create

---
+++ turi_create.py
@@ -0,0 +1,34 @@
+import pandas
+import random
+
+
+
+def main ():
+
+    data_set = pandas.read_csv('dataset.csv')
+
+    print(data_set)
+
+    period = 1000
+    init_model, model = [], []
+    for i in range(3):
+
+        init_model.append(random.random())
+
+    model = init_model.copy()
+
+    for i in range (period):
+        model = algoritmo_option_2(data_set.values.tolist(), 0.1, model)
+
+    print(model)
+
+    for i in range(period):
+        model = algoritmo_option_3(data_set.values.tolist(), 0.1, model)
+
+    print(model)
+
+
+
+
+if __name__ == '__main__':
+    main()
