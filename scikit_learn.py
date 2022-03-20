0c26e403506f828145d2e3a0c0f980c1a48e5e6f
Lepre lepreeeee <i82bocar@uco.es>
2022-03-21 00:58:23 +0100

    Update scikit_learn.py

--- scikit_learn.py
+++ scikit_learn.py
@@ -1,6 +1,36 @@
-import pandas
-import random
+# Tratamiento de datos
+# ==============================================================================
+import pandas as pd
+import numpy as np
 
+# Gráficos
+# ==============================================================================
+import matplotlib.pyplot as plt
+from matplotlib import style
+import seaborn as sns
+
+# Preprocesado y modelado
+# ==============================================================================
+from scipy.stats import pearsonr
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from statsmodels.stats.anova import anova_lm
+from scipy import stats
+
+# Configuración matplotlib
+# ==============================================================================
+plt.rcParams['image.cmap'] = "bwr"
+#plt.rcParams['figure.dpi'] = "100"
+plt.rcParams['savefig.bbox'] = "tight"
+style.use('ggplot') or plt.style.use('ggplot')
+
+# Configuración warnings
+# ==============================================================================
+import warnings
+warnings.filterwarnings('ignore')
 
 def turi_algoritmo():
 
