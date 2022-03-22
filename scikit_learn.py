f8783875d53b7d3253d76e77b22504baa92cbb6e
Lepre lepreeeee <i82bocar@uco.es>
2022-03-22 03:50:15 +0100

    Update scikit_learn.py

--- scikit_learn.py
+++ scikit_learn.py
@@ -1,6 +1,8 @@
 import pandas
 import random
 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 from sklearn import datasets, linear_model
 
 
@@ -19,10 +21,20 @@ def main ():
 
     #cargamos dataset
     data_set = pandas.read_csv('dataset.csv')
-    boston = datasets.load_boston()
-    print(boston)
+
+    x = data_set.drop('y', axis = 1)
+    y = ['y']
+
 
     print(data_set)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    LR = LinearRegression()
+    # fitting the training data
+    LR.fit(x_train, y_train)
+
+    y_prediction = LR.predict(x_test)
+    print(y_prediction)
 
     period = 1000
     init_model, model = [], []
