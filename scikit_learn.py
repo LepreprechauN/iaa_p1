40184c2b6196dac8fa533ba7254914abef256a5a
Lepre lepreeeee <i82bocar@uco.es>
2022-03-23 15:59:54 +0100

    Update scikit_learn.py

--- scikit_learn.py
+++ scikit_learn.py
@@ -6,44 +6,33 @@ from sklearn.linear_model import LinearRegression
 from sklearn import datasets, linear_model
 
 
-def turi_algoritmo():
-
-    modelo = []
-
-
-    return modelo
-
-
-
-
-
 def main ():
 
     #cargamos dataset
     data_set = pandas.read_csv('dataset.csv')
-
-    x = data_set.drop('y', axis = 1)
-    y = ['y']
-
-
     print(data_set)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+
+    x = data_set.drop('y', axis = 1)
+    y = data_set['y']
+
+    print(x,y)
+
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
     LR = LinearRegression()
-    # fitting the training data
+    #fitting the training data
     LR.fit(x_train, y_train)
 
     y_prediction = LR.predict(x_test)
+
     print(y_prediction)
+    print(y_test)
 
-    period = 1000
-    init_model, model = [], []
-    for i in range(3):
 
-        init_model.append(random.random())
-
-    model = init_model.copy()
-
+    print('Pesos: ', LR.coef_)
+    print('Sesgo: ', LR.intercept_)
 
 
 
